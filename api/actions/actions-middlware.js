8bdfb967ca4f5519efbdbc260b7c956c42d44e7f
Visnu <vgcaisse@gmail.com>
2022-04-15 20:26:48 -0400

    got my put and post to work, im on a roll!!! this is a lot easier than i though ong

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,2 +1,31 @@
 // add middlewares here related to actions
 // err i couldnt find a reason to add middlwares here
+const Action = require('./actions-model');
+
+const validateID = (req, res, next) => {
+    Action.get(req.params.id)
+        .then(actions => {
+            if (!actions) {
+                res.status(404).json({ message: `could not retrieve the actions ${req.params.id}` });
+            } else {
+                req.actions = actions;
+                next();
+            }
+        })
+        .catch(err => console.log(err))
+};
+
+const validateBody = (req, res, next) => {
+    const { notes, description, project_id } = req.body;
+
+    if (!notes || !description || !project_id) {
+        res.status(400).json({ message: 'Some fields are missing you goof' });
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    validateID,
+    validateBody
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,48 +1,65 @@
 // Write your "actions" router here!
-const express = require('express');
+const router = require('express').Router();
 const Action = require('./actions-model');
-const router = express.Router();
+
+const { validateID, validateBody } = require('./actions-middlware');
 
 router.get('/', (req, res) => {
-    Action.get()
-        .then(action => {
-            res.status(200).json(action)
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json('*melee announcer*: Failure!!')
-        })
+  Action.get()
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json('*melee announcer*: Failure!!')
+    })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateID, (req, res) => {
   Action.get(req.params.id)
-      .then(action => {
-          if(!action) {
-            res.status(404).json({ message: `what are you 'doing,' haha get it?` })
-          } else {
-            res.status(200).json(action)
-          }
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json('*melee announcer*: Failure!!')
-      })
+    .then(action => {
+      if (!action) {
+        res.status(404).json({ message: `what are you 'doing,' haha get it?` })
+      } else {
+        res.status(200).json(action)
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json('*melee announcer*: Failure!!')
+    })
 })
+
+router.post('/', validateBody, (req, res, next) => {
+  Action.insert(req.body)
+    .then(actions => {
+      res.status(201).json(actions)
+    })
+    .catch(next)
+})
+
+router.put('/:id', validateID, validateBody, (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(next)
+});
 
 router.delete('/:id', (req, res, next) => {
   Action.remove(req.params.id)
-    .then(()=> {
+    .then(() => {
       res.json()
     })
     .catch(next)
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-    res.status(err.status || 500).json({
-      message: err.message,
-      CustomMessage: `le monkes are on it!!`,         // epicn gaben helpin outin
-      stack: err.stack
-    })
+  res.status(err.status || 500).json({
+    message: err.message,
+    CustomMessage: `le monkes are on it!!`,         // epicn gaben helpin outin
+    stack: err.stack
   })
+})
 
 module.exports = router;
